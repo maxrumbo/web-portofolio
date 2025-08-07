ba3e08806a6c1c058b56a878f8937cb8e4d69543
maxrumbo <maxrumbo06@gmail.com>
2025-08-08 01:06:48 +0700

    no admin

--- admin/generate_hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Jalankan file ini sekali saja, lalu copy hasil hash ke database
-$hash = password_hash('maxrumbo', PASSWORD_DEFAULT);
-echo $hash;
-?>
--- hash.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Script untuk generate hash bcrypt dari password "maxrumbo123"
-echo password_hash("maxrumbo123", PASSWORD_BCRYPT);
-?>
